a9e310c9a895d33235eb1c328df696c4f374234a
Tychyna Oleksandr <tychyna.o@gmail.com>
2018-04-07 17:32:09 +0300

    update(*): Prototype and Inheritance

--- js/index.js
+++ js/index.js
@@ -1,22 +1,65 @@
-function Vehicle(brand, color, spead, fuelTankAmount) {
+// ***************************** Vehicle Constructor
+
+function Vehicle(brand, color, speed, fuelTankAmount) {
     this.brand = brand;
-    this.color = color;
+    this._color = color;
 
-    this.spead = spead;
+    this.speed = speed;
     this.fuelTankAmount = fuelTankAmount;
-
-    this.action = function () {
-        console.log('The vehicle ' + this.brand + ' is moving with spead ' + this.spead + ".");
-    }
 }
 
-function Car(brand, color, spead, fuelTankAmoun) {
-    Vehicle.apply(this);
+Vehicle.prototype = {
+    set color(value) {
+        console.log("asdasd 1");
+        this._color = value;
+    },
+    get color() {
+        console.log("asdasd 2");
+        return this._color;
+    },
+};
+
+Vehicle.prototype.action = function () {
+    console.log('The vehicle ' + this.brand + ' is moving with speed ' + this.speed + ".");
 }
 
+Vehicle.prototype.road = function (getDistance, consumption) {
+
+    var averageDistance = 100;
+
+    var time = (getDistance / this.speed) * 60 + " minutes";
+    var fuelConsumption = (getDistance * consumption) / averageDistance;
+    this.fuelTankAmount -= fuelConsumption;
+
+    var data = "<p>Відстань: " + getDistance + " </p>" + "<p>Витрачений час: " + time + " </p>" + "<p>Швидкість: " + this.speed + " </p>" + "<p>Витрачене пальне: " + fuelConsumption + " </p>" + "<p>Залишилось пального: " + this.fuelTankAmount + " </p>";
+
+    orderDocument(data);
+}
+
+function orderDocument(data) {
+    var block = document.getElementById("block");
+    block.innerHTML = data;
+}
+
+// ***************************** Car Constructor
+
+function Car(brand, color, speed, fuelTankAmoun) {
+    Vehicle.apply(this, arguments);
+    Car.count += 1;
+}
 
 Car.prototype = Object.create(Vehicle.prototype);
 Car.prototype.constuctor = Car;
 
-var ferrari = new Car("Ferrari Enzo", "red", 320, 110);
-console.dir(ferrari);
+Car.prototype.checkSteeringWhellSide = function () {
+    Car._random() > 0.5 ? console.log("Left side") : console.log("Right side");
+}
+
+Car.count = 0;
+
+Car._random = function () {
+    return Math.random();
+}
+
+var ferrari = new Vehicle("Ferrari Enzo", "red", 100, 110);
+var bmw = new Car("BMW X5", "black", 125, 130);
